a9435640a02c8e5543c6055fdb2b27ebd45d7660
BtbN <btbn@btbn.de>
2020-09-03 19:34:50 +0200

    Define dummy functions to silence warnings

--- util/vars.sh
+++ util/vars.sh
@@ -9,3 +9,27 @@ TARGET="$1"
 VARIANT="${2:-gpl}"
 REPO="${DOCKER_REPO:-btbn/ffmpeg-builder}"
 IMAGE="$REPO:$TARGET-$VARIANT"
+
+ffbuild_configure() {
+    return 0
+}
+
+ffbuild_unconfigure() {
+    return 0
+}
+
+ffbuild_cflags() {
+    return 0
+}
+
+ffbuild_uncflags() {
+    return 0
+}
+
+ffbuild_ldflags() {
+    return 0
+}
+
+ffbuild_unldflags() {
+    return 0
+}
