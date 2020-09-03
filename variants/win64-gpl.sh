587735e22bf7445df94092560abdd8d332a48d0c
BtbN <btbn@btbn.de>
2020-09-03 18:06:16 +0200

    Install html docs

--- variants/win64-gpl.sh
+++ variants/win64-gpl.sh
@@ -4,6 +4,9 @@ package_variant() {
     IN="$1"
     OUT="$2"
 
-    mkdir -p "$OUT/bin"
+    mkdir -p "$OUT"/bin
     cp "$IN"/bin/* "$OUT"/bin
+
+    mkdir -p "$OUT/doc"
+    cp -r "$IN"/share/doc/ffmpeg/* "$OUT"/doc
 }
