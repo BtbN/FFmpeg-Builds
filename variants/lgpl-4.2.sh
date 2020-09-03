6f3c668b229d89286e21dbc921e6c8206e03023c
BtbN <btbn@btbn.de>
2020-09-03 23:54:05 +0200

    Rely on environment to provide global flags

--- variants/lgpl-4.2.sh
+++ variants/lgpl-4.2.sh
@@ -1,5 +1,5 @@
 FF_CONFIGURE="--enable-version3"
-FF_CFLAGS="-static-libgcc -static-libstdc++"
-FF_CXXFLAGS="-static-libgcc -static-libstdc++"
-FF_LDFLAGS="-static-libgcc -static-libstdc++"
+FF_CFLAGS=""
+FF_CXXFLAGS=""
+FF_LDFLAGS=""
 GIT_BRANCH="release/4.2"
