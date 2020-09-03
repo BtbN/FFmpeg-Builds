9c25ccf05529da1dbe7ee0c9851fa3f07ce1dbef
BtbN <btbn@btbn.de>
2020-09-03 19:40:06 +0200

    Add schannel stub

---
+++ scripts.d/50-schannel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ffbuild_enabled() {
+    [[ $TARGET == win* ]]
+}
+
+ffbuild_dockerstage() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    return 0
+}
+
+ffbuild_configure() {
+    echo --enable-schannel
+}
+
+ffbuild_unconfigure() {
+    echo --disable-schannel
+}
