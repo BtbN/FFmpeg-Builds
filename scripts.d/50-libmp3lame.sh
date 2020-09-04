8e005b17ff3df3bc4ac41a3dbf78a1c35504ee1b
BtbN <btbn@btbn.de>
2020-09-04 20:56:48 +0200

    Add lame

---
+++ scripts.d/50-libmp3lame.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+LAME_SRC="https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz/download"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerstage() {
+    to_df "ADD $SELF /root/lame.sh"
+    to_df "RUN bash -c 'source /root/lame.sh && ffbuild_dockerbuild && rm /root/lame.sh'"
+}
+
+ffbuild_dockerbuild() {
+    mkdir lame
+    cd lame
+    wget -O lame.tar.gz "$LAME_SRC" || return -1
+    tar xaf lame.tar.gz || return -1
+    rm lame.tar.gz
+    cd lame*
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+        --enable-nasm
+        --disable-gtktest
+        --disable-cpml
+        --disable-frontend
+    )
+
+    if [[ $TARGET == win* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    ./configure "${myconf[@]}" || return -1
+    make -j$(nproc) || return -1
+    make install || return -1
+
+    cd ../..
+    rm -rf lame
+}
+
+ffbuild_configure() {
+    echo --enable-libmp3lame
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libmp3lame
+}
