dada1a097a3dbc3ed745f27ed1f44a35f1dcb46f
BtbN <btbn@btbn.de>
2020-09-05 21:50:25 +0200

    Add srt

---
+++ scripts.d/50-srt.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SRT_REPO="https://github.com/Haivision/srt.git"
+SRT_COMMIT="a3f28336a8f0c3c1fce7b2f46d017bcb59af1888"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerstage() {
+    to_df "ADD $SELF /stage.sh"
+    to_df "RUN run_stage"
+}
+
+ffbuild_dockerbuild() {
+    git clone "$SRT_REPO" srt || return -1
+    cd srt
+    git checkout "$SRT_COMMIT" || return -1
+
+    mkdir build && cd build
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DENABLE_SHARED=OFF -DENABLE_STATIC=ON -DENABLE_ENCRYPTION=ON -DENABLE_APPS=OFF .. || return -1
+    make -j$(nproc) || return -1
+    make install || return -1
+
+    cd ../..
+    rm -rf srt
+}
+
+ffbuild_configure() {
+    echo --enable-libsrt
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libsrt
+}
