b4878c4e1f61fa8b65453210f766a74aa1d323cc
BtbN <btbn@btbn.de>
2020-09-05 19:38:50 +0200

    Add zimg

---
+++ scripts.d/50-zimg.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+ZIMG_REPO="https://github.com/sekrit-twc/zimg.git"
+ZIMG_COMMIT="e17ee6cf72509c32c15067b32615849e69486367"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerstage() {
+    to_df "ADD $SELF /stage.sh"
+    to_df "RUN run_stage"
+}
+
+ffbuild_dockerbuild() {
+    git clone "$ZIMG_REPO" zimg || return -1
+    cd zimg
+    git checkout "$ZIMG_COMMIT" || return -1
+
+    ./autogen.sh || return -1
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+    )
+
+    if [[ $TARGET == win* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    ./configure "${myconf[@]}" || return -1
+    make -j$(nproc) || return -1
+    make install || return -1
+
+    cd ..
+    rm -rf zimg
+}
+
+ffbuild_configure() {
+    echo --enable-libzimg
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libzimg
+}
