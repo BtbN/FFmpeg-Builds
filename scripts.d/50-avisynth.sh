e50ce1c132136949e7a38bd8dde4ff4a604527eb
BtbN <btbn@btbn.de>
2020-09-05 16:59:09 +0200

    Add avisynth

---
+++ scripts.d/50-avisynth.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+AVISYNTH_REPO="https://github.com/AviSynth/AviSynthPlus.git"
+AVISYNTH_COMMIT="f44a7c66bfece896c0f0047a5656bb29e73ac67e"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerstage() {
+    to_df "ADD $SELF /stage.sh"
+    to_df "RUN run_stage"
+}
+
+ffbuild_dockerbuild() {
+    git clone "$AVISYNTH_REPO" avisynth || return -1
+    cd avisynth
+    git checkout "$AVISYNTH_COMMIT" || return -1
+
+    mkdir build && cd build
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DHEADERS_ONLY=ON .. || return -1
+    make -j$(nproc) || return -1
+    make install || return -1
+
+    cd ../..
+    rm -rf avisynth
+}
+
+ffbuild_configure() {
+    echo --enable-avisynth
+}
+
+ffbuild_unconfigure() {
+    echo --disable-avisynth
+}
