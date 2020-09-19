4eb20ade1bfdd7c1ff8f23921b7c366a20157edb
BtbN <btbn@btbn.de>
2020-09-20 01:25:41 +0200

    Add script for automatic release pruning

---
+++ util/prunetags.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+git fetch --tags
+TAGS=( $(git tag -l "autobuild-*" | sort -r) )
+
+KEEP_LATEST=14
+KEEP_MONTHLY=12
+
+LATEST_TAGS=()
+MONTHLY_TAGS=()
+
+CUR_MONTH="-1"
+
+for TAG in ${TAGS[@]}; do
+    if [[ ${#LATEST_TAGS[@]} -lt ${KEEP_LATEST} ]]; then
+        LATEST_TAGS+=( "$TAG" )
+    fi
+
+    if [[ ${#MONTHLY_TAGS[@]} -lt ${KEEP_MONTHLY} ]]; then
+        TAG_MONTH="$(echo $TAG | cut -d- -f3)"
+
+        if [[ ${TAG_MONTH} != ${CUR_MONTH} ]]; then
+            CUR_MONTH="${TAG_MONTH}"
+            MONTHLY_TAGS+=( "$TAG" )
+        fi
+    fi
+done
+
+for TAG in ${LATEST_TAGS[@]} ${MONTHLY_TAGS[@]}; do
+    TAGS=( "${TAGS[@]/$TAG}" )
+done
+
+for TAG in ${TAGS[@]}; do
+    echo "Deleting ${TAG}"
+    hub release delete "${TAG}"
+    git tag -d "${TAG}"
+done
+
+git push --tags --prune
