1f31a313c55f8a526719bb4cc69897069366eca9
BtbN <btbn@btbn.de>
2020-10-11 14:43:35 +0200

    Add libuavs3d

---
+++ scripts.d/50-uavs3d.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+UAVS3D_REPO="https://github.com/uavs3/uavs3d.git"
+UAVS3D_COMMIT="fd4c1b4443043f4fe76e10223158e5a1260baf6d"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerstage() {
+    to_df "ADD $SELF /stage.sh"
+    to_df "RUN run_stage"
+}
+
+ffbuild_dockerbuild() {
+    git clone "$UAVS3D_REPO" uavs3d
+    cd uavs3d
+    git checkout "$UAVS3D_COMMIT"
+
+    mkdir build/linux
+    cd build/linux
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_SHARED_LIBS=NO ../..
+    make -j$(nproc)
+    make install
+
+    cd ../..
+    rm -rf uavs3d
+}
+
+ffbuild_configure() {
+    echo --enable-libuavs3d
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libuavs3d
+}
