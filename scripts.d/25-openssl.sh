482f2903d529edb2636ef2386ba63d3a7f44b25b
Nautilus <78522210+sudo-nautilus@users.noreply.github.com>
2021-03-28 16:10:45 +0530

    Update Openssl

--- scripts.d/25-openssl.sh
+++ scripts.d/25-openssl.sh
@@ -1,76 +1,44 @@
 #!/bin/bash
 
 OPENSSL_REPO="https://github.com/openssl/openssl.git"
-<<<<<<< HEAD
-
-=======
->>>>>>> 8f268fa17dc92ff150c6a10e4c0e0512e050d5ca
 OPENSSL_COMMIT="OpenSSL_1_1_1k"
 
 ffbuild_enabled() {
-
     return 0
-
 }
 
 ffbuild_dockerstage() {
-
     to_df "ADD $SELF /stage.sh"
-
     to_df "RUN run_stage"
-
 }
 
 ffbuild_dockerbuild() {
-
     git-mini-clone "$OPENSSL_REPO" "$OPENSSL_COMMIT" openssl
-
     cd openssl
 
     local myconf=(
-
         threads
-
         zlib
-
         no-shared
-
         enable-camellia
-
         enable-ec
-
         enable-srp
-
         --prefix="$FFBUILD_PREFIX"
-
     )
 
     if [[ $TARGET == win64 ]]; then
-
         myconf+=(
-
             --cross-compile-prefix="$FFBUILD_CROSS_PREFIX"
-
             mingw64
-
         )
-
     elif [[ $TARGET == win32 ]]; then
-
         myconf+=(
-
             --cross-compile-prefix="$FFBUILD_CROSS_PREFIX"
-
             mingw
-
         )
-
     else
-
         echo "Unknown target"
-
         return -1
-
     fi
 
     ./Configure "${myconf[@]}" || return -1
@@ -78,11 +46,8 @@ ffbuild_dockerbuild() {
     sed -i -e "/^CFLAGS=/s|=.*|=${CFLAGS}|" -e "/^LDFLAGS=/s|=[[:space:]]*$|=${LDFLAGS}|" Makefile || return -1
 
     make -j$(nproc) || return -1
-
     make install_sw || return -1
 
     cd ..
-
     rm -rf openssl
-
 }
