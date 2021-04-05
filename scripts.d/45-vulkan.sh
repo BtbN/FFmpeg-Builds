2e8328cf23a1f3dfeb9d7ef9ac3a0030c04a8d3e
BtbN <btbn@btbn.de>
2021-04-05 02:20:36 +0200

    Truly statically link Vulkan

--- scripts.d/45-vulkan.sh
+++ scripts.d/45-vulkan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LOADER_REPO="https://github.com/BtbN/Vulkan-Loader.git"
-LOADER_COMMIT="b42b70be453766f56b9ac21c11190b1b5fd74179"
+LOADER_COMMIT="62ff1c9f106a70257cff2fead1bdf1a3c3590239"
 
 ffbuild_enabled() {
     [[ $ADDINS_STR != *vulkan* ]] && return -1
@@ -35,7 +35,7 @@ ffbuild_dockerbuild() {
 
     mkdir build && cd build
 
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_TESTS=OFF ..
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_TESTS=OFF -DBUILD_STATIC_LOADER=ON ..
     make -j$(nproc)
     make install
 }
