b2e9d1a9edef06f00edd3f8a47f52cb1c1624c7f
BtbN <btbn@btbn.de>
2021-04-06 06:23:15 +0200

    Reduce MinGW layer size

--- scripts.d/10-mingw.sh
+++ scripts.d/10-mingw.sh
@@ -9,11 +9,12 @@ ffbuild_enabled() {
 }
 
 ffbuild_dockerlayer() {
-    to_df "COPY --from=${SELFLAYER} /usr/\$FFBUILD_TOOLCHAIN/. /usr/\$FFBUILD_TOOLCHAIN"
+    to_df "COPY --from=${SELFLAYER} /opt/mingw/. /"
+    to_df "COPY --from=${SELFLAYER} /opt/mingw/. /opt/mingw"
 }
 
 ffbuild_dockerfinal() {
-    to_df "COPY --from=${PREVLAYER} /usr/\$FFBUILD_TOOLCHAIN/. /usr/\$FFBUILD_TOOLCHAIN"
+    to_df "COPY --from=${PREVLAYER} /opt/mingw/. /"
 }
 
 ffbuild_dockerbuild() {
@@ -36,7 +37,7 @@ ffbuild_dockerbuild() {
 
     ./configure "${myconf[@]}"
     make -j$(nproc)
-    make install
+    make install DESTDIR="/opt/mingw"
 
     cd ../mingw-w64-libraries/winpthreads
 
@@ -52,7 +53,7 @@ ffbuild_dockerbuild() {
 
     ./configure "${myconf[@]}"
     make -j$(nproc)
-    make install
+    make install DESTDIR="/opt/mingw"
 }
 
 ffbuild_configure() {
