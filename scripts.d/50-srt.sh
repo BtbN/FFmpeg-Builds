05cffc15cfca4dc48ef4979b20fa304f26e04503
BtbN <btbn@btbn.de>
2021-05-13 14:36:08 +0200

    Fix cmake policy issue globally

--- scripts.d/50-srt.sh
+++ scripts.d/50-srt.sh
@@ -14,7 +14,6 @@ ffbuild_dockerbuild() {
     mkdir build && cd build
 
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
-        -DCMAKE_POLICY_DEFAULT_CMP0054=NEW \
         -DENABLE_SHARED=OFF -DENABLE_STATIC=ON -DENABLE_ENCRYPTION=ON -DENABLE_APPS=OFF ..
     make -j$(nproc)
     make install
