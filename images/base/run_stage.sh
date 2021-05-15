186846abf34aca9fa2553ad97c79e8d6e843de4f
BtbN <btbn@btbn.de>
2021-05-15 14:27:46 +0200

    Add support for stage-only flags

--- images/base/run_stage.sh
+++ images/base/run_stage.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -xe
+
+[[ -n "$STAGE_CFLAGS" ]] && export CFLAGS="$CFLAGS $STAGE_CFLAGS"
+[[ -n "$STAGE_CXXFLAGS" ]] && export CXXFLAGS="$CXXFLAGS $STAGE_CXXFLAGS"
+[[ -n "$STAGE_LDFLAGS" ]] && export LDFLAGS="$LDFLAGS $STAGE_LDFLAGS"
+
 mkdir -p /stage
 source "$1"
 cd /stage
