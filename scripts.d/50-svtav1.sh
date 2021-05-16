c171e964a047bef2965145a3937e0f0868f0cf62
N00MKRAD <login@nmkd.de>
2021-05-16 13:37:08 +0200

    Update SVT-AV1 to release 0.8.7
    commit 3971c982311d49f9355dc8dccdcf8d21b70fa624

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SVTAV1_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SVTAV1_COMMIT="0a253a1cec457d50a3a441cec4d553c817bb7231"
+SVTAV1_COMMIT="3971c982311d49f9355dc8dccdcf8d21b70fa624"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
