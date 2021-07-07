dbd238fcdee2f6102a9652899418c146089d55b5
BtbN <btbn@btbn.de>
2021-07-07 14:37:58 +0200

    Add option to skip base image (re)build

--- makeimage.sh
+++ makeimage.sh
@@ -5,11 +5,13 @@ source util/vars.sh
 
 export DOCKER_BUILDKIT=1
 
-if grep "FROM.*base.*" "images/base-${TARGET}/Dockerfile" >/dev/null 2>&1; then
-    docker build --tag "$BASE_IMAGE" images/base
-fi
+if [[ -z "$QUICKBUILD" ]]; then
+    if grep "FROM.*base.*" "images/base-${TARGET}/Dockerfile" >/dev/null 2>&1; then
+        docker build --tag "$BASE_IMAGE" images/base
+    fi
 
-docker build --build-arg GH_REPO="$REPO" --tag "$TARGET_IMAGE" "images/base-${TARGET}"
+    docker build --build-arg GH_REPO="$REPO" --tag "$TARGET_IMAGE" "images/base-${TARGET}"
+fi
 
 ./generate.sh "$TARGET" "$VARIANT" "${ADDINS[@]}"
 
