0de6e6cee7ed1bd1d92878787a2f7510bef719f0
BtbN <btbn@btbn.de>
2021-08-23 00:58:09 +0200

    MinGW patches are gone

--- scripts.d/10-mingw.sh
+++ scripts.d/10-mingw.sh
@@ -8,10 +8,6 @@ ffbuild_enabled() {
     return 0
 }
 
-ffbuild_dockerstage() {
-    to_df "RUN --mount=src=${SELF},dst=/stage.sh --mount=src=patches/mingw,dst=/patches run_stage /stage.sh"
-}
-
 ffbuild_dockerlayer() {
     to_df "COPY --from=${SELFLAYER} /opt/mingw/. /"
     to_df "COPY --from=${SELFLAYER} /opt/mingw/. /opt/mingw"
@@ -24,11 +20,6 @@ ffbuild_dockerfinal() {
 ffbuild_dockerbuild() {
     git-mini-clone "$MINGW_REPO" "$MINGW_COMMIT" mingw
     cd mingw
-
-#    for patch in /patches/*.patch; do
-#        echo "Applying $patch"
-#        git am < "$patch"
-#    done
 
     cd mingw-w64-headers
 
