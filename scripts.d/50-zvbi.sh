69c37865464916e6a0aa54c88766581375f38dc0
BtbN <btbn@btbn.de>
2021-08-23 02:27:46 +0200

    Add libzvbi
    Patches and build-mess based on VLC contrib:
    https://github.com/videolan/vlc/tree/master/contrib/src/zvbi

---
+++ scripts.d/50-zvbi.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+ZVBI_REPO="https://svn.code.sf.net/p/zapping/svn/trunk/vbi"
+ZVBI_REV="4270"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerstage() {
+    to_df "RUN --mount=src=${SELF},dst=/stage.sh --mount=src=patches/zvbi,dst=/patches run_stage /stage.sh"
+}
+
+ffbuild_dockerbuild() {
+    svn checkout "${ZVBI_REPO}@${ZVBI_REV}" zvbi
+    cd zvbi
+
+    for patch in /patches/*.patch; do
+        echo "Applying $patch"
+        patch -p1 < "$patch"
+    done
+
+    autoreconf -i
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+        --with-pic
+        --without-doxygen
+        --without-x
+        --disable-dvb
+        --disable-bktr
+        --disable-nls
+        --disable-proxy
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    ./configure "${myconf[@]}"
+    make -C src -j$(nproc)
+    make -C src install
+    make SUBDIRS=. install
+
+    sed -i "s/\/[^ ]*libiconv.a/-liconv/" "$FFBUILD_PREFIX"/lib/pkgconfig/zvbi-0.2.pc
+}
+
+ffbuild_configure() {
+    echo --enable-libzvbi
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libzvbi
+}
