353ee9e29ca836c9e729a4fcfb57e29a28491e9f
bastimeyer <mail@bastimeyer.de>
2021-08-26 08:31:30 +0200

    build: add license file to archives

--- variants/linux64-nonfree.sh
+++ variants/linux64-nonfree.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source "$(dirname "$BASH_SOURCE")"/linux64-gpl.sh
 FF_CONFIGURE="--enable-nonfree $FF_CONFIGURE"
+LICENSE_FILE=""
--- variants/win32-nonfree.sh
+++ variants/win32-nonfree.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source "$(dirname "$BASH_SOURCE")"/win32-gpl.sh
 FF_CONFIGURE="--enable-nonfree $FF_CONFIGURE"
+LICENSE_FILE=""
