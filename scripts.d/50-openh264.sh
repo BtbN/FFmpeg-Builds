634a3f68acffef61817621c6ab7713fe6729e2ec
BtbN <btbn@btbn.de>
2021-11-13 16:47:42 +0100

    Add OpenH264

---
+++ scripts.d/50-openh264.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+OPENH264_REPO="https://github.com/cisco/openh264.git"
+OPENH264_COMMIT="40913c2a3a10fa3b8dc9e7685214ba1bf19fd11b"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$OPENH264_REPO" "$OPENH264_COMMIT" openh264
+    cd openh264
+
+    mkdir bdir && cd bdir
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --buildtype=release
+        --default-library=static
+        -Dtests=disabled
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --cross-file=/cross.meson
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    meson "${myconf[@]}" ..
+    ninja -j$(nproc)
+    ninja install
+
+    rm "$FFBUILD_PREFIX"/lib/libopenh264.dll.a
+    rm "$FFBUILD_PREFIX"/bin/libopenh264*.dll
+}
+
+ffbuild_configure() {
+    echo --enable-libopenh264
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libopenh264
+}
