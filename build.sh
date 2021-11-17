cf9572ab95019d507d4019c5cca7fcba6100505c
Elaphurus <43885619+Lennox-Elaphurus@users.noreply.github.com>
2021-11-17 10:24:01 +0800

    Change to my FFmpeg repository to build

--- build.sh
+++ build.sh
@@ -50,7 +50,7 @@ rm -f "$TESTFILE"
 rm -rf ffbuild
 mkdir ffbuild
 
-FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/FFmpeg/FFmpeg.git}"
+FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/Lennox-Elaphurus/FFmpeg.git}"
 FFMPEG_REPO="${FFMPEG_REPO_OVERRIDE:-$FFMPEG_REPO}"
 GIT_BRANCH="${GIT_BRANCH:-master}"
 GIT_BRANCH="${GIT_BRANCH_OVERRIDE:-$GIT_BRANCH}"
