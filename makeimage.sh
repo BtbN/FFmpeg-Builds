67d1ae51d6960c5f9a69689a8e364ff4834e85ac
BtbN <btbn@btbn.de>
2021-12-06 19:29:52 +0100

    Explicitly use buildx for local script

--- makeimage.sh
+++ makeimage.sh
@@ -3,16 +3,14 @@ set -xe
 cd "$(dirname "$0")"
 source util/vars.sh
 
-export DOCKER_BUILDKIT=1
-
 if [[ -z "$QUICKBUILD" ]]; then
     if grep "FROM.*base.*" "images/base-${TARGET}/Dockerfile" >/dev/null 2>&1; then
-        docker build --tag "$BASE_IMAGE" images/base
+        docker buildx --builder default build --load --tag "$BASE_IMAGE" images/base
     fi
 
-    docker build --build-arg GH_REPO="$REPO" --tag "$TARGET_IMAGE" "images/base-${TARGET}"
+    docker buildx --builder default build --load --build-arg GH_REPO="$REPO" --tag "$TARGET_IMAGE" "images/base-${TARGET}"
 fi
 
 ./generate.sh "$TARGET" "$VARIANT" "${ADDINS[@]}"
 
-exec docker build --tag "$IMAGE" .
+docker buildx --builder default build --load --tag "$IMAGE" .
