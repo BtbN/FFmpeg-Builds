3938a661be7d8ce2706feb778703b0344447f977
BtbN <btbn@btbn.de>
2021-12-20 15:44:35 +0100

    Add libglvnd

---
+++ scripts.d/45-x11/60-libglvnd.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+GLVND_REPO="https://gitlab.freedesktop.org/glvnd/libglvnd.git"
+GLVND_COMMIT="8f3c5b17a21e2222ab3e5fd38870b915815aca49"
+
+ffbuild_enabled() {
+    [[ $TARGET != linux* ]] && return -1
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$GLVND_REPO" "$GLVND_COMMIT" glvnd
+    cd glvnd
+
+    mkdir build && cd build
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --buildtype=release
+        --default-library=static
+        -Dasm=enabled
+        -Dx11=enabled
+        -Degl=true
+        -Dglx=enabled
+        -Dgles1=true
+        -Dgles2=true
+        -Dheaders=true
+    )
+
+    if [[ $TARGET == linux* ]]; then
+        myconf+=(
+            --cross-file=/cross.meson
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    export LDFLAGS="$LDFLAGS -lxcb -lXau"
+
+    meson "${myconf[@]}" ..
+    ninja -j"$(nproc)"
+    ninja install
+
+    rm "$FFBUILD_PREFIX"/lib/lib*.so*
+}
