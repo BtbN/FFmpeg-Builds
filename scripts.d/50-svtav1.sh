787aa9ab3999dbbfc715927080a9e13e29133144
n00mkrad <login@nmkd.de>
2022-04-13 12:24:29 +0200

    Update to rc2
    Update SVT-AV1 to 1.0.0-rc1

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SVTAV1_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SVTAV1_COMMIT="a9dcf001750ce894187c4bed216675ccc5e038e9"
+SVTAV1_COMMIT="a508cd5386ecf6de8cf2c47a27a3168bcce52907"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
