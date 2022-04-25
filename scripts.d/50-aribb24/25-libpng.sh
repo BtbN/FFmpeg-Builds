e176c338611aef3483e76a1e264447c3ae344ce2
shirt <2660574+shirt-dev@users.noreply.github.com>
2022-04-26 01:54:22 +0200

    Add aribb24 for ARIB caption support
    Closes #152

---
+++ scripts.d/50-aribb24/25-libpng.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+LIBPNG_REPO="https://github.com/glennrp/libpng.git"
+LIBPNG_COMMIT="a37d4836519517bdce6cb9d956092321eca3e73b"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$LIBPNG_REPO" "$LIBPNG_COMMIT" libpng
+    cd libpng
+
+    autoreconf -i
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-shared
+        --enable-static
+        --with-pic
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    export CPPFLAGS="$CPPFLAGS -I$FFBUILD_PREFIX/include"
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install
+}
