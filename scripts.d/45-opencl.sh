89c0e1b8254237d119d04b74566bce54aeae568c
BtbN <btbn@btbn.de>
2022-04-27 13:38:13 +0200

    Update OpenCL-ICD-Loader
    Should be fine to update now, ssee https://github.com/KhronosGroup/OpenCL-ICD-Loader/issues/170

--- scripts.d/45-opencl.sh
+++ scripts.d/45-opencl.sh
@@ -4,7 +4,7 @@ HEADERS_REPO="https://github.com/KhronosGroup/OpenCL-Headers.git"
 HEADERS_COMMIT="dcd5bede6859d26833cd85f0d6bbcee7382dc9b3"
 
 LOADER_REPO="https://github.com/KhronosGroup/OpenCL-ICD-Loader.git"
-LOADER_COMMIT="cd7d07cfa667d8d959b4272be45cf217a65c2948"
+LOADER_COMMIT="aec3952654832211636fc4af613710f80e203b0a"
 
 ffbuild_enabled() {
     return 0
