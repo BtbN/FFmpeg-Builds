e566665ebb3efda2e2e8cce7aedde03d697f7790
bitraid <bitraid@protonmail.ch>
2022-05-25 17:33:35 +0300

    Update SVT-AV1 to 1.1.0

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SVTAV1_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SVTAV1_COMMIT="da60d7c6e27c08806da0b493d5d3fc0a08629102"
+SVTAV1_COMMIT="98aba736053ba0cb00112db88a7f39f16bd64080"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
