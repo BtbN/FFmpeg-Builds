03f047df3a7c8fc5f03641ac95a3ef14fbe84e17
BtbN <btbn@btbn.de>
2022-06-13 23:28:37 +0200

    Fix srt.pc file

--- scripts.d/50-srt.sh
+++ scripts.d/50-srt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SRT_REPO="https://github.com/Haivision/srt.git"
-SRT_COMMIT="fba72ac4d7d5ebf2cad5ddf10ee8fae8953af3f9"
+SRT_COMMIT="e8b713e954345291e8d6e2314d1e5094a6a14179"
 
 ffbuild_enabled() {
     return 0
@@ -14,9 +14,11 @@ ffbuild_dockerbuild() {
     mkdir build && cd build
 
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
-        -DENABLE_SHARED=OFF -DENABLE_STATIC=ON -DENABLE_ENCRYPTION=ON -DENABLE_APPS=OFF ..
+        -DENABLE_SHARED=OFF -DENABLE_STATIC=ON -DENABLE_CXX_DEPS=ON -DUSE_STATIC_LIBSTDCXX=ON -DENABLE_ENCRYPTION=ON -DENABLE_APPS=OFF ..
     make -j$(nproc)
     make install
+
+    echo "Libs.private: -lstdc++" >> "$FFBUILD_PREFIX"/lib/pkgconfig/srt.pc
 }
 
 ffbuild_configure() {
