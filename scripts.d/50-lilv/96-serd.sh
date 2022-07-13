c065001c7101c2ac95591f08f85cd2755893ef52
BtbN <btbn@btbn.de>
2022-07-13 17:07:25 +0200

    Serd switched build system

--- scripts.d/50-lilv/96-serd.sh
+++ scripts.d/50-lilv/96-serd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/drobilla/serd.git"
-SCRIPT_COMMIT="272d7081257dc6d50c06b1b62e8c4baa5bf3b4b2"
+SCRIPT_COMMIT="2465c3f2779617658cf85d3cbc0ca7b786a4ddca"
 
 ffbuild_enabled() {
     return 0
@@ -10,20 +10,30 @@ ffbuild_enabled() {
 ffbuild_dockerbuild() {
     git-mini-clone "$SCRIPT_REPO" "$SCRIPT_COMMIT" serd
     cd serd
-    git submodule update --init --recursive --depth 1
 
-    local mywaf=(
+    mkdir build && cd build
+
+    local myconf=(
         --prefix="$FFBUILD_PREFIX"
-        --no-utils
-        --static
-        --no-shared
-        --largefile
-        --stack-check
+        --buildtype=release
+        --default-library=static
+        -Ddocs=disabled
+        -Dtools=disabled
+        -Dtests=disabled
     )
 
-    CC="${FFBUILD_CROSS_PREFIX}gcc" CXX="${FFBUILD_CROSS_PREFIX}g++" ./waf configure "${mywaf[@]}"
-    ./waf -j$(nproc)
-    ./waf install
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --cross-file=/cross.meson
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    meson "${myconf[@]}" ..
+    ninja -j"$(nproc)"
+    ninja install
 
     sed -i 's/Cflags:/Cflags: -DSERD_STATIC/' "$FFBUILD_PREFIX"/lib/pkgconfig/serd-0.pc
 }
