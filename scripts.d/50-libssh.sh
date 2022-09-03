88fcaaa76f29c4cd6213ae47704508c77db0c460
nanake <nanake@users.noreply.github.com>
2022-09-03 10:52:46 +0900

    libssh: remove workaround
    fixed as https://git.libssh.org/projects/libssh.git/commit/?id=17aec429f539517468446191e3da91df40e352d1
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/50-libssh.sh
+++ scripts.d/50-libssh.sh
@@ -13,11 +13,6 @@ ffbuild_dockerbuild() {
 
     mkdir build && cd build
 
-    if [[ $TARGET == win* ]]; then
-        export CFLAGS="$CFLAGS -Dgettimeofday=ssh_gettimeofday"
-        export CXXFLAGS="$CFLAGS -Dgettimeofday=ssh_gettimeofday"
-    fi
-
     cmake -GNinja -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
         -DBUILD_SHARED_LIBS=OFF \
         -DWITH_EXAMPLES=OFF -DWITH_SERVER=OFF \
