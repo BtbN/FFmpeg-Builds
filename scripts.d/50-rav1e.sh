7d31bb2d06bf297352c0c9cbe2f34f9b259efb6e
BtbN <btbn@btbn.de>
2022-09-12 21:27:05 +0200

    Fix building latest rav1e

--- scripts.d/50-rav1e.sh
+++ scripts.d/50-rav1e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/xiph/rav1e.git"
-SCRIPT_COMMIT="db7a71ae53a31a60bf31bd0635f46e15bdcc444c"
+SCRIPT_COMMIT="25f6c0fadc51f65a0c05ca4540cb8699eba5c644"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
@@ -29,12 +29,9 @@ ffbuild_dockerbuild() {
         )
     fi
 
-    export CC="${FFBUILD_CROSS_PREFIX}gcc"
-    export CXX="${FFBUILD_CROSS_PREFIX}g++"
-    export LD="${FFBUILD_CROSS_PREFIX}gcc"
-    export AR="${FFBUILD_CROSS_PREFIX}ar"
-
     if [[ -n "$FFBUILD_RUST_TARGET" ]]; then
+        unset PKG_CONFIG_LIBDIR
+
         myconf+=(
             --target="$FFBUILD_RUST_TARGET"
         )
@@ -42,8 +39,8 @@ ffbuild_dockerbuild() {
 [build]
 target = "$FFBUILD_RUST_TARGET"
 [target.$FFBUILD_RUST_TARGET]
-linker = "$LD"
-ar = "$AR"
+linker = "${FFBUILD_CROSS_PREFIX}ld"
+ar = "${FFBUILD_CROSS_PREFIX}ar"
 EOF
     fi
 
