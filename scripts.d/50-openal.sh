7028cb01c4140265b055e28692c242b045672186
BtbN <btbn@btbn.de>
2022-10-29 18:50:06 +0200

    Add OpenAL
    Needs fixes to ffmpeg configure before it can be enabled

---
+++ scripts.d/50-openal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/kcat/openal-soft.git"
+SCRIPT_COMMIT="f1f3672f0b8c45b87fcf3b3b99868c274569eaf4"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$SCRIPT_REPO" "$SCRIPT_COMMIT" openal
+    cd openal
+
+    mkdir cm_build && cd cm_build
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DLIBTYPE=STATIC -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF  ..
+    make -j$(nproc)
+    make install
+
+    echo "Libs.private: -lstdc++" >> "$FFBUILD_PREFIX"/lib/pkgconfig/openal.pc
+}
