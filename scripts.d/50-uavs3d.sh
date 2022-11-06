81c3431e572f3b8763fdf6fa7412ef8d9f4435a4
BtbN <btbn@btbn.de>
2022-11-06 12:25:08 +0100

    Enable 10 bit support for uavs3d
    Closes #211

--- scripts.d/50-uavs3d.sh
+++ scripts.d/50-uavs3d.sh
@@ -16,7 +16,8 @@ ffbuild_dockerbuild() {
     mkdir build/linux
     cd build/linux
 
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_SHARED_LIBS=NO ../..
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DCOMPILE_10BIT=1 -DBUILD_SHARED_LIBS=NO ../..
     make -j$(nproc)
     make install
 }
