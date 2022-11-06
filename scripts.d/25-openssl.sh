e7853fda681ded3252f5d49ae6fed813820b257a
BtbN <btbn@btbn.de>
2022-11-06 01:22:37 +0100

    Bump openssl

--- scripts.d/25-openssl.sh
+++ scripts.d/25-openssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/openssl/openssl.git"
-SCRIPT_COMMIT="OpenSSL_1_1_1r"
+SCRIPT_COMMIT="OpenSSL_1_1_1s"
 SCRIPT_TAGFILTER="OpenSSL_1_1_1*"
 
 ffbuild_enabled() {
