2fe73c5aaba608f77060effde14732f7c2aef78b
BtbN <btbn@btbn.de>
2023-07-20 15:29:46 +0200

    Bump avisynth

--- scripts.d/50-avisynth.sh
+++ scripts.d/50-avisynth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/AviSynth/AviSynthPlus.git"
-SCRIPT_COMMIT="ad0d546cd2fd5eadf3b4d29df24bf3f6713d9707"
+SCRIPT_COMMIT="fc5b9bc41fd47001b7da39ea777d29c0ede2a2a7"
 
 ffbuild_enabled() {
     [[ $VARIANT == lgpl* ]] && return -1
