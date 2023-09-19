fe9763a14882422f8d070d5b34ac1c6df3e11ada
BtbN <btbn@btbn.de>
2023-09-19 23:36:02 +0200

    Force true static link for libva-win32

--- scripts.d/50-vaapi/50-libva.sh
+++ scripts.d/50-vaapi/50-libva.sh
@@ -21,18 +21,21 @@ ffbuild_dockerbuild() {
     echo "#include <xcb/xcbext.h>" >> va/x11/va_dri3.c
     echo "xcb_extension_t xcb_dri3_id;" >> va/x11/va_dri3.c
 
+    # Allow to actually toggle static linking
+    sed -i "s/shared_library/library/g" va/meson.build
+
     mkdir mybuild && cd mybuild
 
     local myconf=(
         --prefix="$FFBUILD_PREFIX"
         --buildtype=release
-        --default-library=shared
         -Denable_docs=false
     )
 
     if [[ $TARGET == linux64 ]]; then
         myconf+=(
             --cross-file=/cross.meson
+            --default-library=shared
             --sysconfdir="/etc"
             -Ddriverdir="/usr/lib/x86_64-linux-gnu/dri"
             -Ddisable_drm=false
@@ -43,6 +46,7 @@ ffbuild_dockerbuild() {
     elif [[ $TARGET == win* ]]; then
         myconf+=(
             --cross-file=/cross.meson
+            --default-library=static
             -Dwith_win32=yes
         )
     else
@@ -64,15 +68,6 @@ ffbuild_dockerbuild() {
         rm "$FFBUILD_PREFIX"/lib/libva{,-drm,-x11}.so*
 
         echo "Libs: -ldl" >> "$FFBUILD_PREFIX"/lib/pkgconfig/libva.pc
-    elif [[ $TARGET == win* ]]; then
-        "$FFBUILD_CROSS_PREFIX"gendef - "$FFBUILD_PREFIX"/bin/libva.dll > libva.def
-        "$FFBUILD_CROSS_PREFIX"gendef - "$FFBUILD_PREFIX"/bin/libva_win32.dll > libva_win32.def
-        "$FFBUILD_CROSS_PREFIX"dlltool --input-def libva.def --output-delaylib "$FFBUILD_PREFIX"/lib/libva.a
-        "$FFBUILD_CROSS_PREFIX"dlltool --input-def libva_win32.def --output-delaylib "$FFBUILD_PREFIX"/lib/libva_win32.a
-        rm "$FFBUILD_PREFIX"/bin/libva*.dll "$FFBUILD_PREFIX"/lib/libva*.dll.a
-    else
-        echo "Unknown target"
-        return -1
     fi
 }
 
