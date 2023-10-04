f42e2b82b7909c045f4f190f86510b1f92e5a3b7
DanielM Stanciu <danielm.stanciu@gmail.com>
2023-10-04 13:58:10 +0300

    Update libvpx to 1.3.1
    Libvpx should be update to be more explicit in the fact it includes the security patch.

--- scripts.d/50-libvpx.sh
+++ scripts.d/50-libvpx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://chromium.googlesource.com/webm/libvpx"
-SCRIPT_COMMIT="fd2052d4c91ca9451563482817bbeab1da3f76e7"
+SCRIPT_COMMIT="95cb5eae70d2793d3d05413613aca96042261f26"
 
 ffbuild_enabled() {
     return 0
