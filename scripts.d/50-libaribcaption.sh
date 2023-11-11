9a0acacbd1d8a668528b9f7c32bef09d86eab639
BtbN <btbn@btbn.de>
2023-11-11 19:37:17 +0100

    Add libaribcaption

---
+++ scripts.d/50-libaribcaption.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/xqq/libaribcaption.git"
+SCRIPT_COMMIT="0a3a209d3f0b3650b92941248b9701bf69584a1f"
+
+ffbuild_enabled() {
+    [[ $ADDINS_STR == *4.4* ]] && return -1
+    [[ $ADDINS_STR == *5.0* ]] && return -1
+    [[ $ADDINS_STR == *5.1* ]] && return -1
+    [[ $ADDINS_STR == *6.0* ]] && return -1
+
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    cd "$FFBUILD_DLDIR/$SELF"
+
+    sed -i 's#"-l\.\*#"(-l|/).*#' cmake/GeneratePkgConfig.cmake
+
+    mkdir build
+    cd build
+
+    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DARIBCC_SHARED_LIBRARY=OFF -DARIBCC_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF \
+        -DARIBCC_USE_FREETYPE=ON -DARIBCC_USE_EMBEDDED_FREETYPE=OFF \
+        ..
+
+    ninja -j$(nproc)
+    ninja install
+
+    echo "Libs.private: -lstdc++" >> "$FFBUILD_PREFIX"/lib/pkgconfig/libaribcaption.pc
+}
+
+ffbuild_configure() {
+    echo --enable-libaribcaption
+}
+
+ffbuild_unconfigure() {
+    [[ $ADDINS_STR == *4.4* ]] && return 0
+    [[ $ADDINS_STR == *5.0* ]] && return 0
+    [[ $ADDINS_STR == *5.1* ]] && return 0
+    [[ $ADDINS_STR == *6.0* ]] && return 0
+
+    echo --disable-libaribcaption
+}
