49d31d6bdaf2a5bb7052b21efc194c7179c2cc12
BtbN <btbn@btbn.de>
2023-11-29 18:32:04 +0100

    Switch libpciaccess to meson

--- scripts.d/50-vaapi/30-libpciaccess.sh
+++ scripts.d/50-vaapi/30-libpciaccess.sh
@@ -11,19 +11,18 @@ ffbuild_enabled() {
 ffbuild_dockerbuild() {
     cd "$FFBUILD_DLDIR/$SELF"
 
-    autoreconf -fi
+    mkdir build && cd build
 
     local myconf=(
         --prefix="$FFBUILD_PREFIX"
-        --enable-shared
-        --disable-static
-        --with-pic
-        --with-zlib
+        --buildtype=release
+        --default-library=shared
+        -Dzlib=enabled
     )
 
     if [[ $TARGET == linux* ]]; then
         myconf+=(
-            --host="$FFBUILD_TOOLCHAIN"
+            --cross-file=/cross.meson
         )
     else
         echo "Unknown target"
@@ -33,12 +32,12 @@ ffbuild_dockerbuild() {
     export CFLAGS="$RAW_CFLAGS"
     export LDFLAFS="$RAW_LDFLAGS"
 
-    ./configure "${myconf[@]}"
-    make -j$(nproc)
-    make install
+    meson setup "${myconf[@]}" ..
+    ninja -j$(nproc)
+    ninja install
 
     gen-implib "$FFBUILD_PREFIX"/lib/{libpciaccess.so.0,libpciaccess.a}
-    rm "$FFBUILD_PREFIX"/lib/libpciaccess{.so*,.la}
+    rm "$FFBUILD_PREFIX"/lib/libpciaccess.so*
 
     echo "Libs: -ldl" >> "$FFBUILD_PREFIX"/lib/pkgconfig/pciaccess.pc
 }
