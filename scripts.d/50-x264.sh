bf74c796ffa5632914d80732733c274fe27d6c71
BtbN <btbn@btbn.de>
2024-01-07 18:06:14 +0100

    Switch x264 to official repo

--- scripts.d/50-x264.sh
+++ scripts.d/50-x264.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://github.com/mirror/x264.git"
-SCRIPT_COMMIT="eaa68fad9e5d201d42fde51665f2d137ae96baf0"
+SCRIPT_REPO="https://code.videolan.org/videolan/x264.git"
+SCRIPT_COMMIT="c1c9931dc87289b8aeba78150467f17bdb97d019"
 
 ffbuild_enabled() {
     [[ $VARIANT == lgpl* ]] && return -1
