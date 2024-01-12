961403798edd4cbfcd84b70cedf4b3612dff655b
Sam Hulick <samh@reelcrafter.com>
2024-01-12 17:50:52 -0600

    handle different mktemp syntax

--- makeimage.sh
+++ makeimage.sh
@@ -3,7 +3,13 @@ set -xe
 cd "$(dirname "$0")"
 source util/vars.sh
 
-TMPCFG="$(mktemp --suffix=.toml)"
+export SYSTEM_NAME="$(uname -s)"
+
+if [[ "$SYSTEM_NAME" = "Darwin" ]]; then
+  TMPCFG="$(mktemp -t temp.XXXXXX.toml)"
+else
+  TMPCFG="$(mktemp --suffix=.toml)"
+fi
 cat <<EOF >"$TMPCFG"
 [worker.oci]
   max-parallelism = 4
