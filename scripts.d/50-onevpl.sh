99f2f27e2e8d459169d859b2b952339f12b48c15
Josh Dawkins <zer0x304@gmail.com>
2024-03-09 00:41:44 +0000

    Fix enable onevpl

--- scripts.d/50-onevpl.sh
+++ scripts.d/50-onevpl.sh
@@ -8,8 +8,7 @@ ffbuild_enabled() {
     [[ $ADDINS_STR == *4.4* ]] && return -1
     [[ $ADDINS_STR == *5.0* ]] && return -1
     [[ $ADDINS_STR == *5.1* ]] && return -1
-    # Enable vpl
-    return 1
+    return 0
 }
 
 ffbuild_dockerbuild() {
