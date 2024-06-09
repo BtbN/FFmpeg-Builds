31add5f62d73a618f63993b65e0d67ea76881d54
BtbN <btbn@btbn.de>
2024-06-09 17:21:39 +0200

    Use video-codec-sdk 12.1 for 7.0

--- scripts.d/50-ffnvcodec.sh
+++ scripts.d/50-ffnvcodec.sh
@@ -7,6 +7,10 @@ SCRIPT_REPO2="https://github.com/FFmpeg/nv-codec-headers.git"
 SCRIPT_COMMIT2="22441b505d9d9afc1e3002290820909846c24bdc"
 SCRIPT_BRANCH2="sdk/12.0"
 
+SCRIPT_REPO3="https://github.com/FFmpeg/nv-codec-headers.git"
+SCRIPT_COMMIT3="1889e62e2d35ff7aa9baca2bceb14f053785e6f1"
+SCRIPT_BRANCH3="sdk/12.1"
+
 ffbuild_enabled() {
     [[ $TARGET == winarm64 ]] && return -1
     return 0
@@ -15,11 +19,14 @@ ffbuild_enabled() {
 ffbuild_dockerdl() {
     default_dl ffnvcodec
     echo "git-mini-clone \"$SCRIPT_REPO2\" \"$SCRIPT_COMMIT2\" ffnvcodec2"
+    echo "git-mini-clone \"$SCRIPT_REPO3\" \"$SCRIPT_COMMIT3\" ffnvcodec3"
 }
 
 ffbuild_dockerbuild() {
     if [[ $ADDINS_STR == *4.4* || $ADDINS_STR == *5.0* || $ADDINS_STR == *5.1* || $ADDINS_STR == *6.0* || $ADDINS_STR == *6.1* ]]; then
         cd ffnvcodec2
+    elif [[ $ADDINS_STR == *7.0* ]]; then
+        cd ffnvcodec3
     else
         cd ffnvcodec
     fi
