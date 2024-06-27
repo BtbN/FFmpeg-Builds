2156314438b14a18f08bcacadb59dac5e549c61f
MiKayule <47212579+MiKayule@users.noreply.github.com>
2024-06-28 04:23:49 +0900

    Update 50-svtav1.sh to the latest commit, after v2.1.1
    This commit has passed the official pipeline

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SCRIPT_COMMIT="595a8741b77cab98c7c201b700d6489d240aaeaf"
+SCRIPT_COMMIT="474fec3a391ff0a4d013eba25f0d90f86d04d032"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
