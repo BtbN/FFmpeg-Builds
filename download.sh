18e2e53c23da951b9bb9e1c782f66cbf06737620
BtbN <btbn@btbn.de>
2024-07-19 17:03:49 +0200

    Fix inconsistent tabs/spaces

--- download.sh
+++ download.sh
@@ -19,13 +19,13 @@ trap "rm -rf -- '$DL_SCRIPT_DIR'" EXIT
 mkdir -p "${PWD}"/.cache/downloads
 
 for STAGE in scripts.d/*.sh scripts.d/*/*.sh; do
-    STAGENAME="$(basename "$STAGE" | sed 's/.sh$//')"
+	STAGENAME="$(basename "$STAGE" | sed 's/.sh$//')"
 
 	cat <<-EOF >"${DL_SCRIPT_DIR}/${STAGENAME}.sh"
 		set -xe -o pipefail
 		shopt -s dotglob
 
-        source /dl_functions.sh
+		source /dl_functions.sh
 		source "/$STAGE"
 		STG="\$(ffbuild_dockerdl)"
 
@@ -49,7 +49,7 @@ for STAGE in scripts.d/*.sh scripts.d/*/*.sh; do
 		fi
 
 		WORKDIR="\$(mktemp -d)"
-        trap "rm -rf -- '\$WORKDIR'" EXIT
+		trap "rm -rf -- '\$WORKDIR'" EXIT
 		cd "\$WORKDIR"
 
 		eval "\$STG"
@@ -62,4 +62,4 @@ for STAGE in scripts.d/*.sh scripts.d/*/*.sh; do
 done
 
 docker run -i $TTY_ARG --rm "${UIDARGS[@]}" -v "${DL_SCRIPT_DIR}":/stages -v "${PWD}/.cache/downloads":/dldir -v "${PWD}/scripts.d":/scripts.d -v "${PWD}/util/dl_functions.sh":/dl_functions.sh "${REGISTRY}/${REPO}/base:latest" \
-    bash -c 'set -xe && for STAGE in /stages/*.sh; do bash $STAGE; done'
+	bash -c 'set -xe && for STAGE in /stages/*.sh; do bash $STAGE; done'
