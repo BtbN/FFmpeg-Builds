31f5efbaf772258ee973be58f28ce806970a3f6d
BtbN <btbn@btbn.de>
2024-07-30 20:39:41 +0200

    Revert "Remove xz"
    This reverts commit f3c0a067c96093bf81326952fec78ce543df47fe.

---
+++ scripts.d/25-xz.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/tukaani-project/xz.git"
+SCRIPT_COMMIT="6e8732c5a317a349986a4078718f1d95b67072c5"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    ./autogen.sh --no-po4a --no-doxygen
+
+    local myconf=(
+        --prefix="$FFBUILD_PREFIX"
+        --disable-symbol-versions
+        --disable-shared
+        --enable-static
+        --with-pic
+    )
+
+    if [[ $TARGET == win* || $TARGET == linux* ]]; then
+        myconf+=(
+            --host="$FFBUILD_TOOLCHAIN"
+        )
+    else
+        echo "Unknown target"
+        return -1
+    fi
+
+    ./configure "${myconf[@]}"
+    make -j$(nproc)
+    make install
+}
+
+ffbuild_configure() {
+    echo --enable-lzma
+}
+
+ffbuild_unconfigure() {
+    echo --disable-lzma
+}
