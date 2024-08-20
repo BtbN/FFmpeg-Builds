3ad5c7beb2d3be628667dadcc186287fabf93921
penyuan <penyuan@users.noreply.github.com>
2024-08-20 12:49:53 +0100

    Update 50-svtav1.sh to the latest commit, after v2.2.0
    https://gitlab.com/AOMediaCodec/SVT-AV1/-/releases/v2.2.0

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SCRIPT_COMMIT="21a1154add74504aa86d782a3cf662d3d7deb815"
+SCRIPT_COMMIT="25e0d3e81dcf322abac8690e4eac7bba0cebc054"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
