75548adee9060096c48c22f3f5d85b4649d6d219
BtbN <btbn@btbn.de>
2024-10-03 13:51:37 +0200

    Manually checkout iconvc subcheckout

--- scripts.d/20-libiconv.sh
+++ scripts.d/20-libiconv.sh
@@ -3,13 +3,16 @@
 SCRIPT_REPO="https://git.savannah.gnu.org/git/libiconv.git"
 SCRIPT_COMMIT="0d94621c1e182f5a13a9504523afcb01ec546b37"
 
+SCRIPT_REPO2="https://git.savannah.gnu.org/git/gnulib.git"
+SCRIPT_COMMIT2="eb515396a8506286d5bfa05a9cca4044c1750ff5"
+
 ffbuild_enabled() {
     return 0
 }
 
 ffbuild_dockerdl() {
     echo "retry-tool sh -c \"rm -rf iconv && git clone '$SCRIPT_REPO' iconv\" && git -C iconv checkout \"$SCRIPT_COMMIT\""
-    echo "cd iconv && retry-tool ./autopull.sh --one-time"
+    echo "cd iconv && retry-tool sh -c \"rm -rf gnulib && git clone '$SCRIPT_REPO2' gnulib\" && git -C gnulib checkout \"$SCRIPT_COMMIT2\" && rm -rf gnulib/.git"
 }
 
 ffbuild_dockerbuild() {
