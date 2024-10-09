6f2ecd228d494e610be9f6a3c859d5ebe72ebf71
Defisym <defisym@outlook.com>
2024-10-09 10:54:21 +0800

    Revert `mingw` version
    https://trac.ffmpeg.org/ticket/10833

--- scripts.d/10-mingw.sh
+++ scripts.d/10-mingw.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Source
-SCRIPT_REPO="https://git.code.sf.net/p/mingw-w64/mingw-w64.git"
-SCRIPT_COMMIT="8f7b5ce363fbfa9d66a38034525cf0fdae4385a3"
+# SCRIPT_REPO="https://git.code.sf.net/p/mingw-w64/mingw-w64.git"
+# SCRIPT_COMMIT="8f7b5ce363fbfa9d66a38034525cf0fdae4385a3"
 # Mirror
 # SCRIPT_REPO="https://github.com/mingw-w64/mingw-w64.git"
 # 11.0.0
 # SCRIPT_COMMIT="f9500e2d85b9400c0982518663660a127e1dc61a"
 # 11.0.1
-# SCRIPT_COMMIT="c3e587c067a00a561899d49d3e63a659e38802ec"
+SCRIPT_COMMIT="c3e587c067a00a561899d49d3e63a659e38802ec"
+# Later version has an issue in 6.x
 # 12.0.0
 # SCRIPT_COMMIT="819a6ec2ea87c19814b287e21d65e0dc7f05abba"
 # Latest
