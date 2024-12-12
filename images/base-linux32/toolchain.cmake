b6a172bafa4473a3a85520e866368f012d8c7164
Alibek Omarov <a1ba.omarov@gmail.com>
2024-12-12 23:20:45 +0300

    Add linux32 target
    crosstool-ng is set to build gcc targetting -march=i686 (Pentium Pro), as there
    is little to no sense to support anything lower than that.

---
+++ images/base-linux32/toolchain.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR i686)
+set(CMAKE_SYSTEM_VERSION 4.18.20)
+
+set(triple i686-ffbuild-linux-gnu)
+
+set(CMAKE_C_COMPILER ${triple}-gcc)
+set(CMAKE_CXX_COMPILER ${triple}-g++)
+set(CMAKE_RANLIB ${triple}-gcc-ranlib)
+set(CMAKE_AR ${triple}-gcc-ar)
+
+set(CMAKE_SYSROOT /opt/ct-ng/${triple}/sysroot)
+set(CMAKE_FIND_ROOT_PATH /opt/ct-ng /opt/ct-ng/${triple}/sysroot /opt/ffbuild)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
