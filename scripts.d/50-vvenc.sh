93aace027adccefcc3f4cd7a0647eea54980dc9b
BtbN <btbn@btbn.de>
2024-12-15 17:54:08 +0100

    Disable vvenc due to hard avx2 requirement

--- scripts.d/50-vvenc.sh
+++ scripts.d/50-vvenc.sh
@@ -6,7 +6,10 @@ SCRIPT_COMMIT="7cf1e5ffc5aeb33b81fa9401df9fd53ef1dae6d1"
 ffbuild_enabled() {
     [[ $TARGET != *32 ]] || return -1
     (( $(ffbuild_ffver) > 700 )) || return -1
-    return 0
+    # vvenc force-enabled avx2 and equivalent compiler options, and uses a static initializer that promptly
+    # runs such instructions. Making resulting binaries malfunction on any but the very latest CPUs.
+    # Until upstream fixes this behaviour, force-disable vvenc.
+    return -1
 }
 
 ffbuild_dockerbuild() {
