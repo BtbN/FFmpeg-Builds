2b7d4960b20756ab5249dc30f908b4e39d9ecba4
BtbN <btbn@btbn.de>
2024-12-31 22:48:18 +0100

    Work around svt-av1 vs. libaom symbol collision

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -15,6 +15,8 @@ ffbuild_dockerdl() {
 ffbuild_dockerbuild() {
     mkdir build && cd build
 
+    export CFLAGS="$CFLAGS -Dav1_cospi_arr_s32_data=svt_av1_cospi_arr_s32_data"
+
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_APPS=OFF -DENABLE_AVX512=ON ..
     make -j$(nproc)
     make install
