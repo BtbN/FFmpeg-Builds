75c98f541d96e52b72175e34666fd15509222833
BtbN <btbn@btbn.de>
2025-09-11 18:55:04 +0200

    libdvdnav uses meson now

--- scripts.d/50-dvd/50-libdvdnav.sh
+++ scripts.d/50-dvd/50-libdvdnav.sh
@@ -14,27 +14,31 @@ ffbuild_enabled() {
 }
 
 ffbuild_dockerbuild() {
-    autoreconf -i
+    # stop the static library from exporting symbols when linked into a shared lib
+    sed -i 's/SUPPORT_ATTRIBUTE_VISIBILITY_DEFAULT/SUPPORT_ATTRIBUTE_VISIBILITY_DEFAULT_DISABLED/g' meson.build
+    sed -i 's/-DLIBDVDCSS_EXPORTS//g' src/meson.build
+
+    mkdir build && cd build
 
     local myconf=(
         --prefix="$FFBUILD_PREFIX"
-        --disable-shared
-        --enable-static
-        --with-pic
+        -Ddefault_library=static
+        -Denable_docs=false
+        -Denable_examples=false
     )
 
     if [[ $TARGET == win* || $TARGET == linux* ]]; then
         myconf+=(
-            --host="$FFBUILD_TOOLCHAIN"
+            --cross-file=/cross.meson
         )
     else
         echo "Unknown target"
         return -1
     fi
 
-    ./configure "${myconf[@]}"
-    make -j$(nproc)
-    make install DESTDIR="$FFBUILD_DESTDIR"
+    meson setup "${myconf[@]}" ..
+    ninja -j$(nproc)
+    DESTDIR="$FFBUILD_DESTDIR" ninja install
 }
 
 ffbuild_configure() {
