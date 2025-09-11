50cf63f21151e61c95cebb12994ec8ddac2e2fcc
BtbN <btbn@btbn.de>
2025-09-11 17:23:23 +0200

    Use mirrors for semi-broken gnu git servers

--- scripts.d/20-libiconv.sh
+++ scripts.d/20-libiconv.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://git.savannah.gnu.org/git/libiconv.git"
+SCRIPT_MIRROR="git://git.savannah.gnu.org/libiconv.git"
 SCRIPT_COMMIT="61fbe742a3702e5030f8258ad130a847d6c3e89a"
 
 SCRIPT_REPO2="https://git.savannah.gnu.org/git/gnulib.git"
+SCRIPT_MIRROR2="https://github.com/coreutils/gnulib.git"
 SCRIPT_COMMIT2="ef6d228c7dba8363ce147450c3c2c3ab95642d4c"
 
 ffbuild_enabled() {
@@ -11,8 +13,8 @@ ffbuild_enabled() {
 }
 
 ffbuild_dockerdl() {
-    echo "retry-tool sh -c \"rm -rf iconv && git clone '$SCRIPT_REPO' iconv\" && git -C iconv checkout \"$SCRIPT_COMMIT\""
-    echo "cd iconv && retry-tool sh -c \"rm -rf gnulib && git clone '$SCRIPT_REPO2' gnulib\" && git -C gnulib checkout \"$SCRIPT_COMMIT2\" && rm -rf gnulib/.git"
+    echo "retry-tool sh -c \"rm -rf iconv && git clone '$SCRIPT_MIRROR' iconv\" && git -C iconv checkout \"$SCRIPT_COMMIT\""
+    echo "cd iconv && retry-tool sh -c \"rm -rf gnulib && git clone --filter=blob:none '$SCRIPT_MIRROR2' gnulib\" && git -C gnulib checkout \"$SCRIPT_COMMIT2\" && rm -rf gnulib/.git"
 }
 
 ffbuild_dockerbuild() {
