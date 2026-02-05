07a9ff8bd8ec5ebf5a7e0e2290aecf19ecb51968
Sihu Hwang <129564966+hwangsihu@users.noreply.github.com>
2026-02-05 17:16:53 +0900

    Add new libraries
    - libmysofa (HRTF/sofalizer filter)
    - libspeex (Speex codec)
    - libshine (fixed-point MP3 encoder)
    - libgsm (GSM codec)
    - libbs2b (crossfeed DSP)
    - xevd/xeve (EVC codec, FFmpeg 7.0+)

---
+++ scripts.d/50-evc/55-xeve.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/mpeg5/xeve.git"
+SCRIPT_COMMIT="429c18a7736ffc010e1c550e8015ff18a242d06c"
+
+ffbuild_enabled() {
+    (( $(ffbuild_ffver) >= 700 )) || return -1
+    [[ $TARGET == *32 ]] && return -1
+    return 0
+}
+
+ffbuild_dockerdl() {
+    default_dl .
+    echo "git fetch --unshallow"
+}
+
+ffbuild_dockerbuild() {
+    sed -i '/add_subdirectory(app)/d' CMakeLists.txt
+
+    mkdir ffbuild && cd ffbuild
+
+    local myconf=()
+    if [[ $TARGET == *arm* ]]; then
+        myconf+=(-DARM=TRUE)
+    fi
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        "${myconf[@]}" \
+        ..
+    make -j$(nproc)
+    make install DESTDIR="$FFBUILD_DESTDIR"
+
+    mv "$FFBUILD_DESTPREFIX"/lib/{xeve/libxeve.a,}
+    rm -rf "$FFBUILD_DESTPREFIX"/lib/{libxeve.dll*,libxeve.so*,xeve,xeve_base}
+
+    echo "Cflags.private: -DXEVE_STATIC_DEFINE" >> "$FFBUILD_DESTPREFIX"/lib/pkgconfig/xeve.pc
+}
+
+ffbuild_configure() {
+    (( $(ffbuild_ffver) >= 700 )) || return 0
+    echo --enable-libxeve
+}
+
+ffbuild_unconfigure() {
+    (( $(ffbuild_ffver) >= 700 )) || return 0
+    echo --disable-libxeve
+}
