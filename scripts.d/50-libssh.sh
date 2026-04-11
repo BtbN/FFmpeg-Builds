b9e12aef8b45b9dda78b1f832697e72658fe97fd
BtbN <btbn@btbn.de>
2026-04-11 23:15:09 +0200

    Fix build of libssh on clang as well

--- scripts.d/50-libssh.sh
+++ scripts.d/50-libssh.sh
@@ -16,7 +16,7 @@ ffbuild_enabled() {
 ffbuild_dockerbuild() {
     mkdir build && cd build
 
-    export CFLAGS="$CFLAGS -Dmd5=libssh_md5 -include string.h"
+    export CFLAGS="$CFLAGS -Dmd5=libssh_md5 -std=gnu23 -include string.h"
 
     cmake -GNinja -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
         -DBUILD_SHARED_LIBS=OFF -DWITH_EXAMPLES=OFF -DWITH_SERVER=OFF -DWITH_SFTP=ON -DWITH_ZLIB=ON \
