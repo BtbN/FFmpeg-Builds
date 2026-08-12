77ae23d9a541e37b179c6ac1b5492372f4e9f84c
BtbN <btbn@btbn.de>
2026-08-12 20:53:55 +0200

    Remove fixed pointer warning workaround from lame

--- scripts.d/50-libmp3lame.sh
+++ scripts.d/50-libmp3lame.sh
@@ -39,7 +39,7 @@ ffbuild_dockerbuild() {
         return -1
     fi
 
-    export CFLAGS="$CFLAGS -DNDEBUG -Wno-error=incompatible-pointer-types"
+    export CFLAGS="$CFLAGS -DNDEBUG"
 
     ./configure "${myconf[@]}"
     make -j$(nproc)
