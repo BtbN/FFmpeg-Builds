1bdb68348ff5cf9f344321ba764fb3d4681c053f
BtbN <btbn@btbn.de>
2026-09-23 15:05:07 +0200

    Generate static implibs for glvnd OpenGL libraries

--- scripts.d/45-x11/60-libglvnd.sh
+++ scripts.d/45-x11/60-libglvnd.sh
@@ -36,4 +36,13 @@ ffbuild_dockerbuild() {
     meson "${myconf[@]}" ..
     ninja -j"$(nproc)"
     DESTDIR="$FFBUILD_DESTDIR" ninja install
+
+    for LIB in libGL.so.1 libGLX.so.0 libOpenGL.so.0 libEGL.so.1 libGLESv1_CM.so.1 libGLESv2.so.2; do
+        gen-implib "$FFBUILD_DESTPREFIX"/lib/{"${LIB}","${LIB%%.*}.a"}
+        rm "$FFBUILD_DESTPREFIX"/lib/"${LIB%%.*}".so*
+    done
+
+    for LIB in gl glx opengl egl glesv1_cm glesv2; do
+        echo "Libs: -ldl" >> "$FFBUILD_DESTPREFIX"/lib/pkgconfig/"${LIB}".pc
+    done
 }
